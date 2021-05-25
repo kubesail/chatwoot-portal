7ba22a8732e443aaa98c5695a29b9266d9808a96
Dan Pastusek <dan@hexial.com>
2021-05-25 11:32:56 -0600

    add redirect

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 // @flow
 
-export const API_TARGET =
-  process.env.REACT_APP_API_TARGET || "https://api.kubesail.com";
+export const API_TARGET = process.env.REACT_APP_API_TARGET || "/api";
 export const WWW_TARGET =
   process.env.REACT_APP_WWW_TARGET || "https://platform.kubesail.com";
 export const COMMIT_HASH = process.env.REACT_APP_COMMIT_HASH || "dev";
